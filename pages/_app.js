83527a6cf8526b251b9adecdb225c061dd68f679
Giovanna <giovanna.trigueiro@gmail.com>
2021-07-12 23:12:50 -0300

    :tada: first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,29 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
-
+import { AlurakutStyles } from '../src/lib/AlurakutCommons'
 const GlobalStyle = createGlobalStyle`
-  body {
+  *{
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
+  body {
+    background-color: #252223;
+    font-family: sans-serif;
+  }
+
+  #__next{
+    display: flex;
+    min-height:100vh;
+    flex-direction: column;
+  }
+
+  img{
+    max-width:100%;
+    height:auto;
+    display: block;
+  }
+
+  ${AlurakutStyles}
 `
 
 const theme = {
